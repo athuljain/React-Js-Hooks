0e303e5bd090d944deb1ac413c0539c827caad5a
athuljain <athuljainkj2@gmail.com>
2023-09-11 08:06:26 +0530

    footer commit

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -38,12 +38,10 @@ export default function Footer(){
 
         </div>
       </section>
-  
-         
       <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-        © 2021 Copyright:
-        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+        © 2023 Copyright:
+        <a className='text-reset fw-bold' href='/'>
+          Athul jain
         </a>
       </div>
     </MDBFooter>
